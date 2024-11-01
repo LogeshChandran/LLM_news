650d954a1b920f37cb93395160c8a42b0d4ea04e
Logesh Chandran <60649813+LogeshChandran@users.noreply.github.com>
2024-11-01 18:40:01 +0530

    Update telegram_message_send.py

--- moneycontrol_data_get/telegram_message_send.py
+++ moneycontrol_data_get/telegram_message_send.py
@@ -4,24 +4,45 @@ import pytz
 import logging
 import os
 
+def split_message(text, limit=4096):
+    parts = text.split('\n\n')  # Split by double newline (or adjust to your split logic)
+    chunks = []
+    current_chunk = ""
+
+    for part in parts:
+        if len(current_chunk) + len(part) + 2 <= limit:  # 2 extra for newline between parts
+            current_chunk += part + "\n\n"
+        else:
+            chunks.append(current_chunk.strip())
+            current_chunk = part + "\n\n"
+    
+    if current_chunk:
+        chunks.append(current_chunk.strip())
+    
+    return chunks
+    
 def send_message_with_documents(message="", document_paths=[],captions=[]):
     bot_token = '6511501073:AAHbWvFY_dKcUQfKNGFODOeYK8PEUJ4vXPI'
     chat_id = -1002340142576
     print("telegram message")
     # bot_token = os.environ["BOT_TOKEN"]
     # chat_id = os.environ["CHAT_ID"]
-
-    if message:
-        url = f'https://api.telegram.org/bot{bot_token}/sendMessage'
-        params = {
-            'chat_id': chat_id,
-            'text': message
-        }
-        response = requests.get(url, params=params)
-        if response.status_code == 200:
-            logging.info(f'Message sent successfully! - {message}')
-        else:
-            logging.info(f'Failed to send message: - { response.text }')
+    messages = split_message(message)
+    for msg in messages:
+        if msg :
+        # if message:
+            url = f'https://api.telegram.org/bot{bot_token}/sendMessage'
+            params = {
+                'chat_id': chat_id,
+                # 'text': message
+                'text' : msg
+            }
+            response = requests.get(url, params=params)
+            if response.status_code == 200:
+                # logging.info(f'Message sent successfully! - {message}')
+                logging.info(f'Message sent successfully! - {msg}')
+            else:
+                logging.info(f'Failed to send message: - { response.text }')
 
     document_url = f'https://api.telegram.org/bot{bot_token}/sendDocument'
     for path,caption in zip(document_paths,captions):
