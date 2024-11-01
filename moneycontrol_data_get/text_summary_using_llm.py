220d284b2945c211e6c678c043e9a7cf93894d2c
Logesh Chandran <60649813+LogeshChandran@users.noreply.github.com>
2024-11-01 15:12:14 +0530

    Add files via upload

--- moneycontrol_data_get/text_summary_using_llm.py
+++ moneycontrol_data_get/text_summary_using_llm.py
@@ -33,6 +33,7 @@ def main():
     filtered_df['new summary'] = filtered_df['content'].apply(summarize_content)
     message = "\n\n".join([f"{i+1}. {point}" for i, point in enumerate(filtered_df['new summary'].tolist())])
     telegram_message_send.send_message_with_documents(message)
+    print(message)
     
 if __name__=="__main__":
     main()
