234196ebf8b85fea75cf36f45846e63ca3eda24e
Logesh Chandran <60649813+LogeshChandran@users.noreply.github.com>
2024-11-01 13:56:44 +0530

    Update text_summary_using_llm.py

--- moneycontrol_data_get/text_summary_using_llm.py
+++ moneycontrol_data_get/text_summary_using_llm.py
@@ -2,6 +2,7 @@ import pandas as pd
 from datasets import load_dataset
 from transformers import BartForConditionalGeneration, BartTokenizer
 from datetime import datetime
+import telegram_message_send
 
 dataset = load_dataset("LogeshChandran/money_control_news")
 df = pd.DataFrame(dataset['train'])
@@ -31,6 +32,7 @@ def summarize_content(text):
 def main():
     filtered_df['new summary'] = filtered_df['content'].apply(summarize_content)
     message = "\n\n".join([f"{i+1}. {point}" for i, point in enumerate(filtered_df['new summary'].tolist())])
+    telegram_message_send.send_message_with_documents(message)
     
 if __name__=="__main__":
     main()
